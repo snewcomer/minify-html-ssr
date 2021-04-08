366b620f48c0f8faa089d1eed6847f6c4c92ddd0
scott-newcomer <snewcomer24@gmail.com>
2021-04-07 23:20:15 -0500

    Add bin script

---
+++ src/bin/minify_html_ssr.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let mut src_file = PathBuf::new();
+    src_file.push(args[0].to_owned());
+    let html = std::fs::read_to_string(src_file).unwrap();
+    let res = minify_html_ssr::minify_html(html);
+    println!("count: {}", &res.chars().count());
+    println!("{}", &res);
+}
